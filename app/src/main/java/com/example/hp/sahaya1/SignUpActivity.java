fd3cd8c3d98b61b030e102adca22c32612ff9e9f
Lalitha-Garapati <lalithagarapati4859@gmail.com>
2018-07-17 22:58:33 +0530

    commit

--- app/src/main/java/com/example/hp/sahaya1/SignUpActivity.java
+++ app/src/main/java/com/example/hp/sahaya1/SignUpActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
@@ -23,6 +26,9 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText editTextName, editTextMobile, editTextPassword, editTextAddress, editTextCity,
             editTextDistrict, editTextState;
     private Button buttonSubmit;
+    private Spinner stateSpinner, districtSpinner;
+    String selectedClass, selectedDiv;
+
     private AwesomeValidation awesomeValidation;
     byte[] inputImage;
     Uri image;
@@ -39,9 +45,93 @@ public class SignUpActivity extends AppCompatActivity {
         editTextMobile = (EditText) findViewById(R.id.input_mobile);
         editTextAddress = (EditText) findViewById(R.id.address_location);
         editTextCity = (EditText) findViewById(R.id.address_city);
-        editTextDistrict = (EditText) findViewById(R.id.address_district);
-        editTextState = (EditText) findViewById(R.id.address_state);
+        stateSpinner = (Spinner) findViewById(R.id.address_state);
+        districtSpinner = (Spinner) findViewById(R.id.address_district);
+
+        stateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+
+                String selectedClass = parent.getItemAtPosition(position).toString();
+                switch (selectedClass)
+                {
+                    case "Andhra Pradesh":
+                        // assigning div item list defined in XML to the div Spinner
+                        districtSpinner.setAdapter(new ArrayAdapter<String>(SignUpActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.Andhra)));
+                        break;
+
+                    case "Andaman and Nicobar":
+                        districtSpinner.setAdapter(new ArrayAdapter<String>(SignUpActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.Andaman)));
+                        break;
+
+                   }
+
+                //set divSpinner Visibility to Visible
+                stateSpinner.setVisibility(View.VISIBLE);
+
+                // do something upon option selection
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // can leave this empty
+            }
+        });
+
+        // Div Spinner implementing onItemSelectedListener
+        districtSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+
+                selectedDiv = parent.getItemAtPosition(position).toString();
+                /*
+                    Now that we have both values, lets create a Toast to
+                    show the values on screen
+                */
+                Toast.makeText(SignUpActivity.this, "\n Class: \t " + selectedClass +
+                        "\n Div: \t" + selectedDiv, Toast.LENGTH_LONG).show();
+// do something upon option selection
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // can leave this empty
+            }
+
+        });
+
+       // editTextDistrict = (EditText) findViewById(R.id.address_district);
+        //editTextState = (EditText) findViewById(R.id.address_state);
         buttonSubmit = (Button) findViewById(R.id.btn_signup);
+
+       /* stateSpinner.setEnabled(false);
+        final ArrayAdapter<CharSequence> stateAdapter = ArrayAdapter.createFromResource(this, R.array.States, R.layout.support_simple_spinner_dropdown_item);
+        stateAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        stateSpinner.setAdapter(stateAdapter);
+        stateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] state = stateAdapter.toString().split(" ");
+
+                ArrayAdapter<CharSequence> districtsAdapter = ArrayAdapter.createFromResource(this, R.array.);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });*/
 
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
